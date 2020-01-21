cc0babdff293220e32bab3a579e1b13c29df8045
DongHae <namdh0911@naver.com>
2020-01-21 13:20:36 +0900

    back init

---
+++ back-2team/src/main/java/com/pet/Back2teamApplication.java
@@ -0,0 +1,13 @@
+package com.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back2teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back2teamApplication.class, args);
+	}
+
+}
