db119c4d2e3b224e89c72f443a046648884f02dd
DongHae <namdh0911@naver.com>
2020-01-28 16:49:39 +0900

    pet, user update

--- back-2team/src/main/java/com/pet/sns/model/dao/PetDao.java
+++ back-2team/src/main/java/com/pet/sns/model/dao/PetDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.pet.sns.model.dto.Pet;
 
 public interface PetDao {
+	public List<Pet> testAll();
 	public List<Pet> selectAll(String unum);
 	public Pet selectOne(String petnum);
 	public void insert(Pet p);
