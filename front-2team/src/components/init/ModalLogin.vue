3a485712a7f97af5c2589e4f291b1c8f3580cc0d
Hyeonuk <mrseos@naver.com>
2020-01-30 09:10:05 +0900

    css 수정
    css 수정

--- front-2team/src/components/init/ModalLogin.vue
+++ front-2team/src/components/init/ModalLogin.vue
@@ -1,64 +1,46 @@
 <template>
-<div>
-  <ul class="tabs" role="tablist">
-    <li>
-        <input type="radio" name="tabs" id="tab1" checked />
-        <label for="tab1" 
-               role="tab" 
-               aria-selected="true" 
-               aria-controls="panel1" 
-               tabindex="0">Login</label>
-        <div id="tab-content1" 
-             class="tab-content" 
-             role="tabpanel" 
-             aria-labelledby="description" 
-             aria-hidden="false">
-             <form @submit.prevent="onSubmit(email, password)">
-        <div>
-            <input type="text" v-model="email" placeholder="Email Address" />
-            <input type="submit" value="Login" />
+<div class="contents">
+<div class="tabs" role="tablist">
+    <div class="LoginTab">
+        <input type="radio" name="tabs" id="tab1" checked/>
+        <label for="tab1">Login</label>
+        <div id="tab-content1" class="tab-content">
+            <form @submit.prevent="onSubmit(email, password)">
+                <input type="text" v-model="email" placeholder="Email Address" />
+                <br/>
+                <input type="password" v-model="password" placeholder="Password" />
+                <br/>
+                <input type="submit" value="Login" />
+                <br/>
+            </form>
         </div>
-        <div>
-            <input type="password" v-model="password" placeholder="Password" />
-        </div>
-    </form>
-        </div>
-    </li>
+    </div>
   
-    <li>
-        <input type="radio" name="tabs" id="tab2" />
-        <label for="tab2"
-               role="tab" 
-               aria-selected="false" 
-               aria-controls="panel2" 
-               tabindex="0">Join Us</label>
-        <div id="tab-content2" 
-             class="tab-content"
-             role="tabpanel" 
-             aria-labelledby="specification" 
-             aria-hidden="true">
-             <form @submit="formSubmit">
-        <br/>
-        <input type="email" id="email" name="email" value="e-mail" v-model="name">
-        <input hidden id="interest" name="interest" value="interest" v-model="interest">
-        <input hidden id="phone" name = "phone" value="phone" v-model="phone">
-        <input hidden id="unum" name="unum" value="unum" v-model="unum">
-        <br/>
-        <button>인증메일 받기</button>
-        <br/>
-        <input type="text" id="echeck" name="echeck" value="인증번호 입력" v-model="echeck">
-        <br/>
-        <input type="text" id="nickname" name="nickname" value="nickname" v-model="nickname">
-        <br/>
-        <input type="password" id="password" v-model="password">
-        <br/>
-        <input type="password" id="cpassword">
-        <br/>
-        <input type="submit" value="Login" />
-    </form>
-          </div>
-    </li>
-</ul>
+    <div class="JoinUsTab">
+        <input type="radio" name="tabs" id="tab2"/>
+        <label for="tab2">Join Us</label>
+        <div id="tab-content2" class="tab-content">
+            <form @submit="formSubmit">
+                <input type="email" id="email" name="email" value="e-mail" v-model="name" placeholder="e-mail 입력">
+                <input hidden id="interest" name="interest" value="interest" v-model="interest" placeholder="관심동물 입력">
+                <input hidden id="phone" name = "phone" value="phone" v-model="phone" placeholder="휴대폰 번호 입력">
+                <input hidden id="unum" name="unum" value="unum" v-model="unum">
+                <br/>
+                <button>인증메일 받기</button>
+                <br/>
+                <input type="text" id="echeck" name="echeck" value="인증번호 입력" v-model="echeck" placeholder="이메일 인증번호 입력">
+                <br/>
+                <input type="text" id="nickname" name="nickname" value="nickname" v-model="nickname" placeholder="닉네임 입력">
+                <br/>
+                <input type="password" id="password" v-model="password" placeholder="비밀번호 입력">
+                <br/>
+                <input type="password" id="cpassword" placeholder="비밀번호 한번 더 입력">
+                <br/>
+                <input type="submit" value="회원가입" />
+            </form>
+        </div>
+    </div>
+</div>
 </div>
 </template>
 
@@ -104,101 +86,67 @@ export default {
 </script>
 
 <style scoped>
- @import url("https://fonts.googleapis.com/css?family=Lato");
- * {
-	 margin: 0;
-	 padding: 0;
-	 box-sizing: border-box;
-}
- body {
-	 padding: 20px;
-	 text-align: left;
-	 font-family: Lato;
-	 color: #fff;
-	 background: #9b59b6;
-}
- h1 {
-	 font-weight: normal;
-	 font-size: 40px;
-	 font-weight: normal;
-	 text-transform: uppercase;
-	 float: left;
-	 margin: 20px 0 100px 10px;
-}
- h1 span {
-	 font-size: 13px;
-	 display: block;
-	 padding-left: 4px;
-}
- .tabs {
-	 width: 650px;
-	 float: none;
-	 list-style: none;
-	 position: relative;
-	 margin: 80px 0 0 10px;
-	 text-align: left;
-}
- .tabs li {
-	 float: left;
-	 display: block;
-}
- .tabs input[type="radio"] {
-	 position: absolute;
-	 top: 0;
-	 left: -9999px;
-}
- .tabs label {
-	 display: block;
-	 padding: 14px 21px;
-	 border-radius: 2px 2px 0 0;
-	 font-size: 20px;
-	 font-weight: normal;
-	 text-transform: uppercase;
-	 background: #db81b9;
-	 cursor: pointer;
-	 position: relative;
-	 top: 2px;
-	 transition: all 0.2s ease-in-out;
-}
- .tabs label:hover {
-	 background: #a34186;
-}
- .tabs .tab-content {
-	 z-index: 2;
-	 display: none;
-	 overflow: hidden;
-	 width: 100%;
-	 font-size: 17px;
-	 line-height: 25px;
-	 padding: 25px;
-	 position: absolute;
-	 top: 53px;
-	 left: 0;
-	 background: #c54092;
-}
- .tabs [id^="tab"]:checked + label {
-	 top: 0;
-	 padding-top: 17px;
-	 background: #c54092;
-}
- .tabs [id^="tab"]:checked ~ [id^="tab-content"] {
-	 display: block;
-}
- p.link {
-	 clear: both;
-	 margin: 380px 0 0 15px;
-}
- p.link a {
-	 text-transform: uppercase;
-	 text-decoration: none;
-	 display: inline-block;
-	 color: #fff;
-	 padding: 5px 10px;
-	 margin: 0 5px;
-	 background-color: #612e76;
-	 transition: all 0.2s ease-in;
-}
- p.link a:hover {
-	 background-color: #522764;
-}
+    .contents { /* 화면 중앙 정렬*/
+        position:absolute;
+        top:0;right:0;bottom:20%;left:0;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        display:-webkit-flex;
+        -webkit-align-item:center;
+        -webkit-justify-content:center;
+    }
+
+    .tabs { /* 탭 초기 세팅 */
+        float: none;
+        list-style: none;
+        position: absolute;
+        vertical-align: middle;
+        text-align: left;
+    }
+
+    .tabs div { /* 탭 내용 감싸기 */
+        float: left;
+        display: block;
+    }
+
+    .tabs input[type="radio"] { /* 라디오 버튼을 탭 모양으로! */
+        position: absolute;
+        top: 20px; /* 라디오 버튼 가리기용*/
+    }
+
+    .tabs label { /* 탭 제목 */
+        display: block;
+        padding: 15px 20px; /* 높이 너비 */
+        border-radius: 20% 20% 0 0;
+        font-size: 15px;
+        font-weight: normal;
+        background: #e275b8;
+        cursor: pointer;
+        position: relative;
+        top: 2px;
+    }
+
+    .tabs label:hover {
+        background: #cf68b0; /* 탭 부분 마우스올렸을 때 색상바꿈 */
+    }
+
+    .tabs .tab-content { /* 활성화 된 탭 부분 */
+        display: none;
+        width: 100%;
+        font-size: 15px;
+        padding: 20%;
+        position: absolute;
+        left: 0;
+        background: #c54092; 
+    }
+
+    .tabs [id^="tab"]:checked + label { /* 클릭된 탭 부분 색상 */
+        background: #c54092; 
+    }
+
+    .tabs [id^="tab"]:checked ~ [id^="tab-content"] { /* 클릭되지 않은 부분 비활성화 */
+        display: block;
+    }
+    
 </style>
